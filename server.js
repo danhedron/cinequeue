226f279f8389947bedf34aa31fe31b336d6a4218
Douglas Gardner <douglas@chippy.ch>
2014-05-17 14:22:54 +0100

    Add i18n, split files, add 404
    * Move main entry point from `index.js` to `server.js`
    * Add dependencies for `cookie-parser` and `i18n`
    * Rearrange `server.js` variable initialization order
    * Make jade pretty-print everywhere
    * Add localisation
    * Add prettily formatted error handling
    * Add 404 page
    * Move routes from `server.js` to their own subdirectory
    * Move center align from body to form in CSS
    * Scope selectors
    * Add skeleton jade file
    * Modify footer to be multiple p tags
    * Add URL decoding where appropriate

---
+++ server.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var app = module.exports = express();
+
+var os = require('os');
+var spawn = require('child_process').spawn;
+var i18n = require('i18n');
+
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jade')
+app.use(express.static(__dirname + '/static'))
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+
+app.locals.pretty = true; // set jade to pretty-print globally
+
+i18n.configure({
+	locales: ['en', 'de' ],
+	directory: __dirname + '/i18n',
+	defaultLocale: 'en',
+	cookie: 'cinequeue-locale'
+});
+
+app.use(i18n.init);
+
+var server = app.listen(9000, function() {
+	console.log('Listening on port %d', server.address().port);
+});
+
+// routes (etc): TODO: load this dynamically
+var players = require('./routes/player');
+var languages = require('./routes/langselect');
+var errors = require('./routes/errors');
+
+
