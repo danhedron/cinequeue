danhedron/cinequeue

commit 849f67e3991b6053d6eb6102dec960bd9bc74f44

Add and fix JSHint warnings
* Add assorted JSHint warnings, notably:
	- Restrict line length to a hard limit of 120
	- Force use of apostrophes for strings, not quotation marks
* Add (unused) `interactive` variable in `lint.js`
* Use more sensible alias for `lint.js`'s git call

routes/error.js
@@ -4,6 +4,7 @@ var i18n = require( 'i18n' ); // needed for console messages
 app.use( function ( req, res, next ) {
 	res.status( 404 );
 	res.render( 'notfound' );
+	next();
 } );
 
 app.use( function ( err, req, res, next ) {
@@ -14,4 +15,5 @@ app.use( function ( err, req, res, next ) {
 	res.render( 'servererror', {
 		err: err
 	} );
+	next();
 } );

.jshintrc
@@ -6,5 +6,14 @@
   "quotmark": true,
   "undef": false,
   "unused": true,
-  "trailing": true
+  "trailing": true,
+  "camelcase": true,
+  "eqeqeq": true,
+  "newcap": true,
+  "noarg": true,
+  "noempty": true,
+  "nonbsp": true,
+  "nonew": true
+  "quotmark": "single",
+  "maxlen": 120
 }

config.js
@@ -114,7 +114,7 @@ try {
 	process.exit( 1 );
 }
 
-if ( conf.get( 'web.secret' ) == conf.default( 'web.secret' ) ) {
+if ( conf.get( 'web.secret' ) === conf.default( 'web.secret' ) ) {
 	colog.warning( 'Using default secret!' );
 }
 

scripts/lint.js
@@ -4,8 +4,9 @@
 var exec = require( 'execSync' ).exec;
 var log = require( 'colog' );
 var buffer = [];
+var interactive = false;
 
-var git = 'git diff-index --name-only --cached HEAD --';
+var git = 'git diff-index --name-only --staged HEAD --';
 git = exec( git );
 
 if ( !git.code ) {
@@ -20,21 +21,23 @@ if ( !git.code ) {
 		log.headerWarning( 'Lint failed.' );
 		log.warning( buffer );
 
-		/*
-		var rl = readline.createInterface( {
-			input: process.stdin,
-			output: process.stdout
-		} );
-		rl.question( log.cyan( 'Continue? [y/N]' )  , function ( answer ) {
-			if ( answer.toUpperCase().substr(0) == 'Y' ) {
-				log.answer( 'Continuing.' );
-				process.exit( 0 );
-			} else {
-				log.answer( 'Cancelling.' );
-				process.exit( 1 );
-			}
-		} );
-		*/
+		if ( interactive ) {
+			// this was intended for the pre-commit hook, but it abstracts the
+			// output away unless there's an error, so the prompt will just hang
+			var rl = readline.createInterface( {
+				input: process.stdin,
+				output: process.stdout
+			} );
+			rl.question( log.cyan( 'Continue? [y/N]' )  , function ( answer ) {
+				if ( answer.toUpperCase().substr(0) === 'Y' ) {
+					log.answer( 'Continuing.' );
+					process.exit( 0 );
+				} else {
+					log.answer( 'Cancelling.' );
+					process.exit( 1 );
+				}
+			} );
+		}
 		process.exit( 1 );
 	} else {
 		log.success( 'Lint successful!' );

server.js
@@ -40,8 +40,8 @@ app.use( function ( req, res, next ) {
 // pretty print jade
 app.locals.pretty = true;
 
-var messages = require( './messages' ); // load i18n
-var routes = require( './routes' );
+require( './messages' ); // load i18n
+require( './routes' );
 
 app.listen( config.get( 'web.port' ) );
 colog.success( i18n.__( 'Listening on port %d', config.get( 'web.port' ) ) );

commit 0554142d7a08853d91c2311a1892ce2e6f652cea

Fix regression
`/playlist/` failed to render in a timely fashion.
This bug was introduced in 849f67e3991b6053d6eb6102dec960bd9bc74f44.

Removed `next()` call, all should now be well.

routes/error.js
@@ -1,10 +1,9 @@
 var log = require( 'colog' );
 var app = require( '..' );
 var i18n = require( 'i18n' ); // needed for console messages
-app.use( function ( req, res, next ) {
+app.use( function ( req, res ) {
 	res.status( 404 );
 	res.render( 'notfound' );
-	next();
 } );
 
 app.use( function ( err, req, res, next ) {
@@ -15,5 +14,5 @@ app.use( function ( err, req, res, next ) {
 	res.render( 'servererror', {
 		err: err
 	} );
-	next();
+	next;
 } );