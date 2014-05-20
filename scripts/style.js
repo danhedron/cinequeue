9e9da5d053473bb13e2afa4f5befdf2fb1e780e5
Douglas Gardner <douglas@chippy.ch>
2014-05-21 00:32:35 +0100

    fix regex

--- scripts/style.js
+++ scripts/style.js
@@ -2,17 +2,19 @@ var fs = require( 'fs' );
 var log = require( 'colog' );
 var git = require( './git' );
 var pass = true;
+var regex = /\(([^ ].*|[& ])\)/;
 
 git.getStaged( function ( error, files ) {
 	for ( var i = 0; i < files.length; i++ ) {
 		console.log( files[i] );
 		if ( ( files[i].indexOf( '.js', files[i].length - 3 ) ) !== -1 ) {
 			var contents = fs.readFileSync( files[i] ).toString();
-			if ( contents.match( /\([^ ]+\)/ ) ) {
+			if ( contents.match( regex ) ) {
 				pass = false;
 				contents = contents.split( '\n' );
 				for ( var j = 0; j < contents.length; j++ ) {
-					if ( contents[j].match( /\([^ ]+\)/ ) ) {
+					if ( contents[j].match( regex ) ) {
+						console.log('X' + contents[j].match( regex ) );
 						log.error( 'Badly formed parentheses in ' +  files[i] + ' on line ' + ( j + 1 ) );
 						log.warning( '\t' + contents[j] );
 					}
